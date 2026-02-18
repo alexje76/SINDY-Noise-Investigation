6c8644e41ecae8dc7c136d4aa0117318b800d013
alexje76 <155279139+alexje76@users.noreply.github.com>
2026-02-17 19:04:34 -0800

    Created Module for functions

---
+++ sindy-recreate-module.py
@@ -0,0 +1,17 @@
+"""
+Module that contains all the functions we need for our project.
+"""
+#IMPORTS
+import numpy as np
+
+#FUNCTIONS
+def placeholder_function()
+  pass
+
+#CLasses
+class PlaceholderClass:
+  pass
+
+#Runtime info
+if __name__ == '__main__': 
+    main()
